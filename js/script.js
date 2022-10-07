306c42a94ee8bc260448883e328accf771646a1a
ValentinaZussino <vzussino@protonmail.com>
2022-10-07 15:26:54 +0200

    impostazione prima milestone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('eccoci')
+
+const btnCalc = document.getElementById('btn-calcola');
+const km = document.getElementById('km').value;
+const age = document.getElementById('age').value;
+let priceForNumbKm = 0.21 * km;
+console.log(km, age, priceForNumbKm)
+
+const ticketPrice = function(){
+        if(age < 18){
+            priceForNumbKm = priceForNumbKm * 0.8;
+        } else if(age > 65){
+            priceForNumbKm = priceForNumbKm * 0.6;
+        } else {
+            priceForNumbKm = priceForNumbKm;
+        }
+        console.log(priceForNumbKm)
+}
+
+btnCalc.addEventListener('click', ticketPrice);
